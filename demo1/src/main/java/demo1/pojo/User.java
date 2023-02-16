a1485ffa19aea86bff8d6b8a4e578bb09b744ffc
criticoo <3495701774@qq.com>
2023-02-16 16:29:10 +0800

    1 commit

---
+++ demo1/src/main/java/demo1/pojo/User.java
@@ -0,0 +1,23 @@
+package demo1.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @projectName: Git_Demo
+ * @package: demo1.pojo
+ * @className: User
+ * @author: White
+ * @description: TODO
+ * @date: 2023/2/16 16:24
+ * @version: 1.0
+ */
+
+@Data
+public class User implements Serializable {
+
+    private Long userId;
+
+    private String userName;
+}
